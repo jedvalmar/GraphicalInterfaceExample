94ee1fd6ebc5d1d8aa5926ce26ce4c41fa0677cc
Eduardo Valenzuela <lalo@ievolutioned.com>
2014-03-24 20:49:28 -0600

    Initial Commit

---
+++ GraphicalInterfaceExample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GraphicalInterfaceExample
+//
+//  Created by Eduardo Valenzuela on 3/24/14.
+//  Copyright (c) 2014 Eduardo Valenzuela. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GraphicalInterfaceExampleTests/GraphicalInterfaceExampleTests.m
@@ -0,0 +1,34 @@
+//
+//  GraphicalInterfaceExampleTests.m
+//  GraphicalInterfaceExampleTests
+//
+//  Created by Eduardo Valenzuela on 3/24/14.
+//  Copyright (c) 2014 Eduardo Valenzuela. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface GraphicalInterfaceExampleTests : XCTestCase
+
+@end
+
+@implementation GraphicalInterfaceExampleTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
